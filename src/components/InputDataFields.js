d67cd95c0ca9d1108918c14485a312b824eed42d
turcuciprian <turcuciprian1@gmail.com>
2020-05-20 14:18:11 +0300

    implemented a basic styled components wrappers  removed some default css stuff  added styled components package

--- src/components/InputDataFields.js
+++ src/components/InputDataFields.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  InputFieldsWrapper,
+  TaraWrapper,
+  OrasWrapper,
+} from "../other/styledComponents";
 const InputDataFields = () => {
-  return <>Fields go here</>;
+  const [oras, setOras] = useState("Cluj Napoca");
+  return (
+    <>
+      <InputFieldsWrapper>
+        <p>Seteaza un oras si o tara:</p>
+        <TaraWrapper>
+          Tara:{" "}
+          <select>
+            <option value="ro">Romania</option>
+          </select>
+        </TaraWrapper>
+        <OrasWrapper>
+          Oras:{" "}
+          <input
+            Type="text"
+            onChange={(e) => setOras(e.target.value)}
+            value={oras}
+          />
+        </OrasWrapper>
+        <button>Actualizeaza Vremea</button>
+      </InputFieldsWrapper>
+    </>
+  );
 };
 export default InputDataFields;
---
+++ src/other/styledComponents.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InputFieldsWrapper = styled.div`
+  font-size: 14px;
+  background-color: lightblue;
+  padding: 10px;
+  border: 1px solid #c3c3c3;
+  color: #000;
+`;
+export const TaraWrapper = styled.div`
+  font-size: 14px;
+  background-color: lightblue;
+`;
+export const OrasWrapper = styled.div`
+  font-size: 14px;
+  background-color: lightblue;
+`;
