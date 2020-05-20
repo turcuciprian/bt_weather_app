1057c0822fb5c478af66cc0cb6c4b038a818fe52
turcuciprian <turcuciprian1@gmail.com>
2020-05-20 14:20:45 +0300

    styled input component a little

--- src/other/styledComponents.js
+++ src/other/styledComponents.js
@@ -6,12 +6,18 @@ export const InputFieldsWrapper = styled.div`
   padding: 10px;
   border: 1px solid #c3c3c3;
   color: #000;
+  button{
+      background:lightgreen;
+      padding:5px 10px;
+      font-size:18px;
+  }
 `;
 export const TaraWrapper = styled.div`
   font-size: 14px;
   background-color: lightblue;
 `;
 export const OrasWrapper = styled.div`
+margin:10px 0 10px 0;
   font-size: 14px;
   background-color: lightblue;
 `;
